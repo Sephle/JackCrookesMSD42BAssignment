e8450e5597adc5303b1e526a8476914b627fa920
Sephle <73096391+Sephle@users.noreply.github.com>
2021-01-02 23:30:06 +0100

    Task 1: A-E

---
+++ HomeAssignment/Car Game/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    //Scrolling Speed
+    [SerializeField] float backgroundScrollSpeed = 0.02f;
+    //Material Texture
+    Material myMaterial;
+    //offSet movement
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets the material
+        myMaterial = GetComponent<Renderer>().material;
+        //scrolls in the y axis at speed
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves the material by offSet every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
---
+++ HomeAssignment/Car Game/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Player Speed
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xCamMin, xCamMax, yCamMin, yCamMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        //Setting Up boundaries
+        Camera gameCamera = Camera.main;
+
+        xCamMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xCamMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yCamMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yCamMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Move()
+    {
+        //Update deltaX with movement left and right
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+
+        newXPos = Mathf.Clamp(newXPos, xCamMin, xCamMax);
+
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+    }
+}
