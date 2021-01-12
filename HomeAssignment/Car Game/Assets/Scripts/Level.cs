359133469e3adac3072eb127886800d01d3060fe
Sephle <73096391+Sephle@users.noreply.github.com>
2021-01-12 23:08:39 +0100

    Added Level Script and Fixed Car Collision

---
+++ HomeAssignment/Car Game/Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    public void LoadStartMenu()
+    {
+        //First Scene
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("CarGame");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
