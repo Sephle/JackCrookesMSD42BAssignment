91c6ccb2cf937454fb908b0bbba395548d5891a3
Sephle <73096391+Sephle@users.noreply.github.com>
2021-01-15 04:34:37 +0100

    Layer Collision & Point System

---
+++ HomeAssignment/Car Game/Assets/Scripts/GameSession.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    //Deletes one of the other GameSession if there is one
+    private void SetUpSingleton()
+    {
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+
+        if (numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+
+        score += scoreValue;
+        print(score);
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ HomeAssignment/Car Game/Assets/Scripts/ObstacleShredder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleShredder : MonoBehaviour
+{
+    [SerializeField] int scoreValue = 5;
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        Destroy(otherObject.gameObject);
+
+        //Adds up to Score
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+    }
+}
